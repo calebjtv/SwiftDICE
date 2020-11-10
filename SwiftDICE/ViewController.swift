2dd12dbc6a74e1cb0ae09bc1544ef23a36149bcb
calebjtv <calebjtv@gmail.com>
2020-11-09 20:45:35 -0600

    it works now

--- SwiftDICE/ViewController.swift
+++ SwiftDICE/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var diceImageView: UIImageView!
+    @IBOutlet var criticalLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonGotPressed()
+    {
+        rollDice()
+    }
+    
+    func rollDice()
+    {
+        print("We rollin")
+        
+        let rolledNumber = Int.random(in: 1...20)
+        
+        let imageName="dice\(rolledNumber)"
+        
+        diceImageView.image = UIImage(named: imageName)
+        
+        if (imageName == "dice1")
+        {
+            criticalLabel.text = "Crit Fail"
+        
+        }
+        else if (imageName == "dice20")
+        {
+            criticalLabel.text = "Crit Success!"
+        }
+        else
+        {
+            criticalLabel.text = "yeehaw"
+        }
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollDice()
+    }
+    
 }
 
